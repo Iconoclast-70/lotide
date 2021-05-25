db222ddd4c8ffdfb235b1cf9f5086b113594c9c1
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-05-25 01:10:45 +0000

    Head Function

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(hArray) {
+  if (hArray.length > 0) {
+    return hArray.shift()
+  } else {
+    return hArray;
+  }
+}
+
+assertEqual(head([3,4]), 4);
+assertEqual(head(["Goodbye","Hello"]), "Hello");
