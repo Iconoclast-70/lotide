db9463972093e196735034c711db36902d5e53a8
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-05-27 22:43:06 +0000

    Take Until Callback

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(arr1,arr2) {
+  let arraysEqual = true;
+  if (arr1.length === arr2.length) {
+    for (let a1 = 0; a1 < arr1.length; a1++) {
+      if (arr1[a1] !== arr2[a1]) {
+        arraysEqual = false;
+      }
+    }
+  } else {
+    arraysEqual = false;
+  }
+
+  if (arraysEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+  return arraysEqual;
+};
+
+const takeUntil = function(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0,i);
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+console.log(assertArraysEqual(data1,results1));
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+console.log(assertArraysEqual(data2,results2));
