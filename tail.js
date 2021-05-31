d3ba757ac99b26f4d5cfb239e1cebc5f2884c0ee
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-05-31 20:25:35 +0000

    Updated Tail

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(tArray) {
   if (tArray.length > 0) {
     let vTail = tArray.slice(1);
@@ -15,10 +7,6 @@ const tail = function(tArray) {
   }
 };
 
-const result = tail(["Hello","Lighthouse","Labs"]);
-assertEqual(result.length,2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1],"Labs");
-
+module.exports = tail;
   
 
