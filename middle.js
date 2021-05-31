e45b7f1eb1c10cec34f833730705c5e11aa984a1
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-05-31 21:52:48 +0000

    Updated middle function

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1,arr2) {
-  let arraysEqual = true;
-  if (arr1.length === arr2.length) {
-    for (let a1 = 0; a1 < arr1.length; a1++) {
-      if (arr1[a1] !== arr2[a1]) {
-        arraysEqual = false;
-      }
-    }
-  } else {
-    arraysEqual = false;
-  }
-
-  if (arraysEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
-
 const middle = function(arr) {
   if (arr.length > 2) {
     if (arr.length % 2 === 0) {
@@ -28,9 +8,4 @@ const middle = function(arr) {
   }
 };
 
-const middleEven = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
-const middleOdd = [1,2,3];
-
-console.log(middle(middleEven));
-console.log(middle(middleOdd));
-console.log(eqArrays(middle(middleEven),middle(middleOdd)));
+module.exports = middle;
